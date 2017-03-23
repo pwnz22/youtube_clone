f097dc431980a652205e8348e29dc0039fe016b4
Valijon <parliament888@gmail.com>
2017-03-24 01:41:45 +0800

    Getting information by token

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client as Guzzle;
 use Illuminate\Http\Request;
 use App\Repositories\UserRepository;
 
 class HomeController extends Controller
 {
+    protected $client;
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Guzzle $client)
     {
         $this->middleware('auth');
+        $this->client = $client;
     }
 
     /**
@@ -24,8 +27,21 @@ class HomeController extends Controller
      */
     public function index(Request $request, UserRepository $users)
     {
+        $tweets = collect();
+        if ($request->user()->token) {
+            $response = $this->client->get('http://api_auth/api/tweets', [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer ' . $request->user()->token->access_token
+                ]
+            ]);
+
+            $tweets = collect(json_decode($response->getBody()));
+        }
+
         $subscriptionVideos = $users->videosFromSubscriptions($request->user());
 
-        return view('home', compact('subscriptionVideos'));
+        return view('home', compact('subscriptionVideos', 'tweets'));
     }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,25 @@
                             </div>
                         @endforeach
                     @else
-                        You are logged in.
+                        @if(Auth::user()->token)
+                            @if($tweets->count())
+                                @foreach($tweets as $tweet)
+                                    <div class="media">
+                                        <div class="media-left">
+                                            <a href="#">
+                                                <img class="media-object" src="https://placeholdit.imgix.net/~text?txtsize=8&txt=65%C3%9765&w=65&h=65" alt="">
+                                            </a>
+                                        </div>
+                                        <div class="media-body">
+                                            <strong>{{ $tweet->user->name }}</strong>
+                                            <p>{{ $tweet->body }}</p>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            @endif
+                        @else
+                            <p>Please <a href="{{ url('/oauth/twitter') }}">authorize with Twitter</a></p>
+                        @endif
                     @endif
                 </div>
             </div>
